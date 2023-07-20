544773b3d6921dc6b98bfcf3a2f09c30c21223d8
andrey <13rred@gmail.com>
2023-07-20 23:07:45 +0300

    task0303(Seminar3task3)

---
+++ task0303/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает 
+// диапазон возможных координат точек в этой четверти (x и y)
+
+int Coordinata(string message) // пишем передаваемое сообщение - запрашиваем у пользователя число
+{
+    Console.WriteLine(message + " ");
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+
+void FindQuarter(int a) // отправляем возможные координаты и выводим сообщением
+
+{
+    if (a < 1 || a > 4 )
+        Console.WriteLine("неверный формат данных");
+    if (a == 1)
+        Console.WriteLine("X = (0; +n), Y = (0; +n)");
+
+    if (a == 2)
+        Console.WriteLine("X = (0; -n), Y = (0; +n)");
+
+    if (a == 3)
+        Console.WriteLine("X = (0; -n), Y = (0; -n)");
+
+    if (a == 4)
+        Console.WriteLine("X = (0; +n), Y = (0; -n)");
+}
+
+int quarter = Coordinata("Введите номер четвверти: ");
+FindQuarter(quarter);
+
