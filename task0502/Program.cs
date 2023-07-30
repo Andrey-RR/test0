8140361231d46f2a83a2e8c6e0dce7f7015decc3
andrey <13rred@gmail.com>
2023-07-31 00:55:56 +0300

    task0502

---
+++ task0502/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 32: Напишите программу замена элементов массива:
+//  положительные элементы замените на соответствующие отрицательные, и наоборот. 
+//  [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+        // Console.Write($"{result[i]} "); используется для проверки вывода
+    }
+    return result;
+}
+
+int GetNum(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
